e17e49837aa6504f4c87a61d03b3f7fdd30491be
soohykeee <gur1028@skuniv.ac.kr>
2022-08-21 15:12:12 +0900

    220815 8강 복습 다익스트라, 플로이드와샬, 냅색, 크루스칼, 유니온파인드

---
+++ src/ch08/floydWarshall01.java
@@ -0,0 +1,65 @@
+package ch08;
+
+/*
+플로이드 와샬 알고리즘
+N개의 도시가 주어지고, 각 도시들을 연결하는 도로와 해당 도로를 통행하는 비용이 주어질
+때 모든 도시에서 모든 도시로 이동하는데 쓰이는 비용의 최소값을 구하는 프로그램을 작성하세요.
+
+모든 정점에서 모든 정점으로의 최단 경로를 구하고 싶을 떄 사용하는 알고리즘
+거쳐가는 정점을 기준으로 알고리즘을 수행
+ */
+
+public class floydWarshall01 {
+
+    public int solution(int n, int[][] edges) {
+
+        // 비용을 저장하는 dy 이차원 배열 초기화.
+        int[][] dy = new int[n + 1][n + 1];
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (i == j) {
+                    dy[i][j] = 0;
+                } else {
+                    dy[i][j] = 100000;
+                }
+            }
+        }
+
+        // 연결된 간선의 비용을 갱신
+        for (int[] x : edges) {
+            dy[x[0]][x[1]] = x[2];
+        }
+
+        // 중간에 k를 거쳐 도달 정점으로 갈때 원래 비용보다 최소라면 새로 값을 갱신
+        for (int k = 1; k <= n; k++) {
+            for (int i = 1; i <= n; i++) {
+                for (int j = 1; j <= n; j++) {
+                    // i~j로 가는길에 k를 거쳐 가는 경로
+                    if (dy[i][k] + dy[k][j] < dy[i][j]) {
+                        dy[i][j] = dy[i][k] + dy[k][j];
+                    }
+                }
+            }
+        }
+
+        // 답 출력
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (dy[i][j] == 100000) {
+                    System.out.print("M ");
+                } else {
+                    System.out.print(dy[i][j] + " ");
+                }
+            }
+            System.out.println();
+        }
+
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        floydWarshall01 T = new floydWarshall01();
+        System.out.println(T.solution(5, new int[][]{{1, 2, 6}, {1, 3, 3}, {3, 2, 2},
+                {2, 4, 1}, {2, 5, 13}, {3, 4, 5}, {4, 2, 3}, {4, 5, 7}}));
+    }
+}
---
+++ src/ch08/kruskal01.java
@@ -0,0 +1,82 @@
+package ch08;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+/*
+원더랜드(최소스패닝트리 : 크루스칼, Union&Find 활용)
+원더랜드에 문제가 생겼다. 원더랜드의 각 도로를 유지보수하는 재정이 바닥난 것이다.
+원더랜드는 모든 도시를 서로 연결하면서 최소의 유지비용이 들도록 도로를 선택하고 나머지
+도로는 폐쇄하려고 한다.
+아래의 그림은 그 한 예를 설명하는 그림이다.
+
+위의 지도는 각 도시가 1부터 9로 표현되었고, 지도의 오른쪽은 최소비용 196으로 모든 도시
+를 연결하는 방법을 찾아낸 것이다.
+
+크루스칼 알고리즘이란?
+그래프 내의 모든 정점들을 가장 적은 비용으로 연결하기 위해 사용
+-> 그래프 내의 모든 정점을 포함하고 사이클이 없는 연결 선을 그렸을 때, 가중치의 합이
+최소가 되는 상황을 구하고 싶을 때 사용한다. (최소 신장 트리를 구현하기 위한 알고리즘) + 사이클 x
+ */
+class Edge1 implements Comparable<Edge1> {
+    public int v1;
+    public int v2;
+    public int cost;
+
+    Edge1(int v1, int v2, int cost) {
+        this.v1 = v1;
+        this.v2 = v2;
+        this.cost = cost;
+    }
+
+    @Override
+    public int compareTo(Edge1 o) {
+        return this.cost - o.cost;
+    }
+}
+
+public class kruskal01 {
+    int[] unf;
+
+    public int Find(int v) {
+        if(v==unf[v]) return v;
+        else return unf[v]=Find(unf[v]);
+    }
+
+    public void Union(int a, int b) {
+        int fa = Find(a);
+        int fb = Find(b);
+
+        if(fa != fb) unf[fa] = fb;
+    }
+
+    public int solution(int n, int[][] edges) {
+        unf = new int[n + 1];
+        ArrayList<Edge1> list = new ArrayList<>();
+        for (int i = 1; i <= n; i++) unf[i] = i;
+
+        for (int[] x : edges) {
+            list.add(new Edge1(x[0], x[1], x[2]));
+        }
+
+        int answer = 0;
+        Collections.sort(list);
+        for (Edge1 ob : list) {
+            int fv1 = Find(ob.v1);
+            int fv2 = Find(ob.v2);
+            if (fv1 != fv2) {
+                answer += ob.cost;
+                Union(ob.v1, ob.v2);
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        kruskal01 T = new kruskal01();
+
+        System.out.println(T.solution(9, new int[][]{{1, 2, 12}, {1, 9, 25}, {2, 3, 10}, {2, 8, 17}, {2, 9, 8},
+                {3, 4, 18}, {3, 7, 55}, {4, 5, 44}, {5, 6, 60}, {5, 7, 38}, {7, 8, 35}, {8, 9, 15}}));
+    }
+
+}
