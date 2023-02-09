f05f321462fa16e728cfbf962ccfc52f76fd3d92
soohykeee <gur1028@skuniv.ac.kr>
2023-02-09 23:40:49 +0900

    230209 프로그래머스 연습문제

---
+++ src/programmers/exerciseLv2/구명보트.java
@@ -0,0 +1,21 @@
+package programmers.exerciseLv2;
+
+import java.util.Arrays;
+
+public class 구명보트 {
+
+    public int solution(int[] people, int limit) {
+        int answer = 0;
+
+        Arrays.sort(people);
+
+        int min = 0;
+
+        for (int max = people.length - 1; min <= max; max--){
+            if (people[min] + people[max] <= limit) min++;
+            answer++;
+        }
+
+        return answer;
+    }
+}
