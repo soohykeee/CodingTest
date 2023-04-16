4a16fb74c6cb9538718deeda8739ebe6a2857a21
soohykeee <gur1028@skuniv.ac.kr>
2023-04-16 14:08:51 +0900

    230416 프로그래머스 연습문제 lv.2 clean code

---
+++ src/programmers/exerciseLv2/뉴스클러스터링.java
@@ -0,0 +1,85 @@
+package programmers.exerciseLv2;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+/*
+뉴스 클러스터링 - 2018 KAKAO BLIND RECRUITMENT
+
+ */
+public class 뉴스클러스터링 {
+    public static boolean isAlpha(String s) {
+        if (s == null) {
+            return false;
+        }
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (!(c >= 'A' && c <= 'Z') && !(c >= 'a' && c <= 'z')) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public int solution(String str1, String str2) {
+
+        ArrayList<String> strList1 = new ArrayList<>();
+        ArrayList<String> strList2 = new ArrayList<>();
+        ArrayList<String> gyo = new ArrayList<>();
+        ArrayList<String> hap = new ArrayList<>();
+
+        str1 = str1.toUpperCase();
+        str2 = str2.toUpperCase();
+
+        // 2글자씩 나눠 ArrayList에 저장
+        for (int i = 0; i < str1.length() - 1; i++) {
+            String temp = str1.substring(i, i + 2);
+            if (isAlpha(temp)) {
+                strList1.add(temp);
+            }
+        }
+        for (int i = 0; i < str2.length() - 1; i++) {
+            String temp = str2.substring(i, i + 2);
+            if (isAlpha(temp)) {
+                strList2.add(temp);
+            }
+        }
+
+        // 중복원소 처리 위한 정렬
+        Collections.sort(strList1);
+        Collections.sort(strList2);
+
+        // 교집합 구하기
+        for (String s : strList1) {
+            if (strList2.remove(s)) {
+                gyo.add(s);
+            }
+            hap.add(s);
+        }
+        // 합집합 구하기
+        for (String s : strList2) {
+            hap.add(s);
+        }
+
+        // 합집합이 0 이라면 return 65536
+        if (hap.size() == 0) {
+            return 65536;
+        }
+
+        double answer = (double) gyo.size() / (double) hap.size();
+        return (int) (answer * 65536);
+
+    }
+
+    public static void main(String[] args) {
+        뉴스클러스터링 T = new 뉴스클러스터링();
+
+        System.out.println(T.solution("FRANCE", "french"));
+        System.out.println(T.solution("handshake", "shake hands"));
+        System.out.println(T.solution("aa1+aa2", "AAAA12"));
+        System.out.println(T.solution("E=M*C^2", "e=m*c^2"));
+        System.out.println(T.solution("abab", "baba"));
+
+    }
+}
--- src/programmers/exerciseLv2/프린터.java
+++ src/programmers/exerciseLv2/프린터.java
@@ -3,6 +3,10 @@ package programmers.exerciseLv2;
 import java.util.Collections;
 import java.util.PriorityQueue;
 
+/*
+프린터 - 연습문제 lv.2
+
+ */
 public class 프린터 {
 
     public int solution(int[] priorities, int location) {
@@ -11,8 +15,8 @@ public class 프린터 {
         //Collections.reverseOrder() 를 해줘야 최대부터 꺼낸다.
         PriorityQueue<Integer> queue = new PriorityQueue<>(Collections.reverseOrder());
 
-        for (int i = 0; i < priorities.length; i++) {
-            queue.add(priorities[i]);
+        for (int x : priorities) {
+            queue.add(x);
         }
 
         while (!queue.isEmpty()) {
@@ -29,5 +33,13 @@ public class 프린터 {
         }
 
         return answer;
+    }
+
+    public static void main(String[] args) {
+        프린터 T = new 프린터();
+
+        System.out.println(T.solution(new int[]{2, 1, 3, 2}, 2));
+        System.out.println(T.solution(new int[]{1, 1, 9, 1, 1, 1}, 0));
+
     }
 }
