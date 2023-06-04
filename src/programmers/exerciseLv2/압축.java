c9ca1113c3a27fe26cd751dec31fb6eb71a92408
soohykeee <gur1028@skuniv.ac.kr>
2023-06-04 17:33:18 +0900

    230604 프로그래머스 연습문제 lv.2

--- src/programmers/exerciseLv2/압축.java
+++ src/programmers/exerciseLv2/압축.java
@@ -1,5 +1,8 @@
 package programmers.exerciseLv2;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 /*
 [3차] 압축 - 2018 KAKAO BLIND RECRUITMENT
 
@@ -8,10 +11,34 @@ public class 압축 {
 
     public int[] solution(String msg) {
         int[] answer = {};
+        int count = 0;
+        HashMap<String, Integer> hashMap = new HashMap<>();
+
+        for (int i = 1; i < 27; i++) {
+            char alpha = (char) (64+i);
+            hashMap.put(String.valueOf(alpha), i);
+        }
+
+        for (int i = 0; i < msg.length(); i++) {
+//            addLZW(hashMap, msg.);
+        }
+
 
 
         return answer;
     }
+
+    public int addLZW(HashMap<String, Integer> hashMap, String str, int count) {
+
+        if (!hashMap.containsKey(str)) {
+            hashMap.put(str, count);
+            return count;
+        }
+
+        return hashMap.get(str);
+
+    }
+
 
     public static void main(String[] args) {
         압축 T = new 압축();
