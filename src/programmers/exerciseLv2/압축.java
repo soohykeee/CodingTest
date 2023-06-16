4e0a7a8b10563464bfa4a561a22ab51a73218b51
soohykeee <gur1028@skuniv.ac.kr>
2023-06-16 23:26:32 +0900

    230616 프로그래머스 연습문제 lv.2

--- src/programmers/exerciseLv2/압축.java
+++ src/programmers/exerciseLv2/압축.java
@@ -11,32 +11,43 @@ public class 압축 {
 
     public int[] solution(String msg) {
         int[] answer = {};
-        int count = 0;
-        HashMap<String, Integer> hashMap = new HashMap<>();
+        ArrayList<Integer> al = new ArrayList<>();
+        String w = "";
+        String c = "";
 
-        for (int i = 1; i < 27; i++) {
-            char alpha = (char) (64+i);
-            hashMap.put(String.valueOf(alpha), i);
+        HashMap<String, Integer> map = new HashMap<>();
+        for (int i = 1; i <= 26; i++) {
+            String temp = (char) ('A' + (i - 1)) + "";
+            map.put(temp, i);
         }
 
-        for (int i = 0; i < msg.length(); i++) {
-//            addLZW(hashMap, msg.);
+        for (int i = 0; i < msg.length(); ) {
+            w = msg.charAt(i) + "";
+
+            for (int j = i + 1; j < msg.length(); j++) {
+                w += msg.charAt(j);
+                if (!map.containsKey(w)) {
+                    c = msg.substring(j, j + 1);
+                    w = w.substring(0, w.length() - 1);
+                    break;
+                }
+            }
+
+            i += w.length();
+            al.add(map.get(w));
+
+            if (c.length() > 0) {
+                String s = w + c;
+                map.put(s, map.size() + 1);
+            }
         }
 
-
+        answer = new int[al.size()];
+        for (int i = 0; i < al.size(); i++) {
+            answer[i] = al.get(i);
+        }
 
         return answer;
-    }
-
-    public int addLZW(HashMap<String, Integer> hashMap, String str, int count) {
-
-        if (!hashMap.containsKey(str)) {
-            hashMap.put(str, count);
-            return count;
-        }
-
-        return hashMap.get(str);
-
     }
 
 
